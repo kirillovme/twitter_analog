9103e835b383fc85917552d9615b0740fe043771
Dmitry Kirillov <popts60@gmail.com>
2020-02-06 19:05:15 +0300

    First commit

---
+++ src/main/java/com/example/page/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package com.example.page.repos;
+
+import com.example.page.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+}
